4726061aa5d6b7fc46eb0ba57034eb2aa76e40f4
jay0jim <284096905@qq.com>
2022-10-15 17:26:00 +0800

    实现记录session_id功能

--- HelloWorld/pages/login/login.js
+++ HelloWorld/pages/login/login.js
@@ -1,8 +1,9 @@
 // pages/login.js
+// import {createRequestHeader} from '../../request_header'
 
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 var app = getApp()
-
+var utils = require('../../utils/kiwiRequest.js')
 
 Page({
 
@@ -15,36 +16,58 @@ Page({
     session_id: '',
   },
 
-  loginTest() {
+  onQuickLogin() {
+    // 先使用session登录，如果没有session或已过期，则再请求res_code后再登录
+    utils.kiwiRequestWithSessionId({
+      url: 'https://api.kiwistudio.work/kiwi/user/login',
+      method: 'POST',
+      data:{
+        's': 's'
+      },
+      success: (r, session_id) => {
 
+        // 登录出错
+        if (r.data.code == 200) {
+          // 登录成功
+          console.log(r.data.msg, session_id)
+          
+        } else {
+          
+          // 提示需请求res_code再登录
+          if (r.data.code == 400200) {
+            console.log('需要重新登陆')
+            this.loginTest()
+          }
+
+          console.log(r)
+        }
+      },
+    })
+  },
+
+  loginTest() {
+    var that = this
     wx.login({
       success: (res) => {
-        if(res.code) {
-          var session_id = app.globalData.session_id
+        if (res.code) {
           var data_dic = {
             res_code: res.code,
           }
-          if(session_id != null) {
-            data_dic['session_id'] = session_id
-          }
-
-
-          wx.request({
-            url: 'https://test.kiwistudio.work/kiwi/user/login',
+          
+          utils.kiwiRequestWithSessionId({
+            url: 'https://api.kiwistudio.work/kiwi/user/login',
             data: data_dic,
-            success: (r) => {
-              console.log(r.data.session_id)
-              // 把session_id保存到本地
-              app.globalData.session_id = r.data.session_id
-              wx.setStorageSync('session_id', r.data.session_id)
+            success: (r, session_id) => {
+              console.log(r.data.msg, session_id)
 
               that.setData({
-                session_id: r.data.session_id
+                session_id: session_id
               })
             },
             method: 'POST'
           })
-          
+
+
         } else {
           console.log('获取用户登录态失败！' + res.errMsg)
         }
@@ -55,7 +78,9 @@ Page({
   },
 
   onChooseAvatar(e) {
-    const { avatarUrl } = e.detail 
+    const {
+      avatarUrl
+    } = e.detail
     console.log(avatarUrl)
     this.setData({
       avatarUrl,
@@ -65,14 +90,54 @@ Page({
 
   onNetworkTest() {
     const that = this
+    console.log(that.data.session_id)
+    var header = {
+      'Cookie': this.data.session_id
+    }
+
     console.log('请求后台')
     wx.request({
-      url: 'https://test.kiwistudio.work/kiwi/test',
+      url: 'https://api.kiwistudio.work/kiwi/test',
+      header: header,
       data: {
         info: 'from mini-program'
       },
       success: (res) => {
-        console.log(res)
+        var cookie = res.header['Set-Cookie']
+        if (cookie != null) {
+          if (cookie.indexOf('session=') != -1) {
+            // 使用正则表达式把session=XXXXX提取出来
+            // 并保存到全局变量和本地中
+            var session_id = cookie.match("\\bsession=([^;]*)\\b")[0]
+
+            app.globalData.session_id = session_id
+            wx.setStorageSync('session_id', session_id)
+
+            that.setData({
+              session_id: session_id
+            })
+          }
+
+        }
+      },
+      method: 'POST'
+    })
+  },
+
+  onNewTest() {
+
+    utils.kiwiRequestWithSessionId({
+      url: 'https://api.kiwistudio.work/kiwi/test',
+      data: {
+        info: 'from mini-program'
+      },
+      success: (res, session_id) => {
+        var that = this
+        console.log(session_id)
+
+        that.setData({
+          session_id: session_id
+        })
       },
       method: 'POST'
     })
